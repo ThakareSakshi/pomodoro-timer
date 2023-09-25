d93e96bf85f2b3189c92ba987750e338e850393c
Navendu Singh <54157440+NavenduSingh07@users.noreply.github.com>
2023-09-25 16:07:00 +0530

    Updated index.js
    added js of setting and modified previous js to make changes in UI

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 class Timer {
     constructor(root) {
       root.innerHTML = Timer.getHTML();
@@ -12,13 +10,16 @@ class Timer {
         shortB: root.querySelector(".timer__btn--short"),
         longB: root.querySelector(".timer__btn--long"),
         pomodoro: root.querySelector(".timer__btn--pomodoro"),
-
-
-        
       };
   
       this.interval = null;
-      this.remainingSeconds = 25*60;
+      this.settings = {
+        sessionDuration: 25 * 60,  // Default session duration in seconds
+        breakDuration: 5 * 60,    // Default break duration in seconds
+        longBreakAfter: 4,        // Default number of Pomodoros before a long break
+        longBreakDuration: 15 * 60 // Default long break duration in seconds
+      };
+      this.remainingSeconds = this.settings.sessionDuration;
       this.updateInterfaceTime();
   
       this.el.control.addEventListener("click", () => {
@@ -28,22 +29,23 @@ class Timer {
           this.stop();
         }
       });
+  
       this.el.shortB.addEventListener("click", () => {
-        this.remainingSeconds = 5*60;
+        this.remainingSeconds = this.settings.breakDuration;
         this.updateInterfaceTime();
         document.getElementById("hero-section").style.backgroundColor = "rgb(247,195,152)";
       });
+  
       this.el.longB.addEventListener("click", () => {
-        this.remainingSeconds = 15*60;
+        this.remainingSeconds = this.settings.longBreakDuration;
         this.updateInterfaceTime();
         document.getElementById("hero-section").style.backgroundColor = "rgb(168,216,167)";
-
       });
+  
       this.el.pomodoro.addEventListener("click", () => {
-        this.remainingSeconds = 25*60;
+        this.remainingSeconds = this.settings.sessionDuration;
         this.updateInterfaceTime();
         document.getElementById("hero-section").style.backgroundColor = "rgb(200,221,248)";
-
       });
   
       this.el.reset.addEventListener("click", () => {
@@ -51,7 +53,8 @@ class Timer {
   
         if (inputMinutes < 60) {
           this.stop();
-          this.remainingSeconds = inputMinutes * 60;
+          this.settings.sessionDuration = inputMinutes * 60;
+          this.remainingSeconds = this.settings.sessionDuration;
           this.updateInterfaceTime();
         }
       });
@@ -64,7 +67,6 @@ class Timer {
       this.el.minutes.textContent = minutes.toString().padStart(2, "0");
       this.el.seconds.textContent = seconds.toString().padStart(2, "0");
     }
-   
   
     updateInterfaceControls() {
       if (this.interval === null) {
@@ -86,10 +88,9 @@ class Timer {
         this.updateInterfaceTime();
   
         if (this.remainingSeconds === 0) {
-          
           var audio = new Audio('./mixkit-data-scaner-2847.wav');
-         audio.play();
-         this.stop();
+          audio.play();
+          this.stop();
         }
       }, 1000);
   
@@ -98,9 +99,7 @@ class Timer {
   
     stop() {
       clearInterval(this.interval);
-  
       this.interval = null;
-  
       this.updateInterfaceControls();
     }
   
@@ -108,37 +107,62 @@ class Timer {
       return `
       <div class="time-btn">
       <button class="btn timer__btn--pomodoro">Pomodoro</button>
-      <button class="btn timer__btn--short" >Short Break</button>
+      <button class="btn timer__btn--short">Short Break</button>
       <button class="btn timer__btn--long">Long Break</button>
-  </div>
-            <div class="time">
-            <span class="timer__part timer__part--minutes">00</span>
-            <span> Min</span>
-            <span class="timer__part">:</span>
-             <span class="timer__part timer__part--seconds">00</span>
-             <span> Sec </span>
-            </div>
-            <div class="timer-toggle-btn">
-            <button type="button" class="timer__btn timer__btn--control timer__btn--start">
-            <span class="material-icons">play_arrow</span>
-            </button>
-            <button type="button" class="timer__btn timer__btn--reset">
-            <span class="material-icons">timer</span>
-            </button>
-             </div>
-            </div>
-          `;
+    </div>
+      <div class="time">
+      <span class="timer__part timer__part--minutes">00</span>
+      <span> Min</span>
+      <span class="timer__part">:</span>
+      <span class="timer__part timer__part--seconds">00</span>
+      <span> Sec </span>
+      </div>
+      <div class="timer-toggle-btn">
+      <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+      <span class="material-icons">play_arrow</span>
+      </button>
+      <button type="button" class="timer__btn timer__btn--reset">
+      <span class="material-icons">timer</span>
+      </button>
+      </div>
+    `;
     }
-   
-    // shortBreak(){
-    //     this.remainingSeconds = 5* 60;
-    //     this.updateInterfaceTime();
-    // }
+  
+    setSettings(sessionDuration, breakDuration, longBreakAfter, longBreakDuration) {
+      this.settings.sessionDuration = sessionDuration * 60;
+      this.settings.breakDuration = breakDuration * 60;
+      this.settings.longBreakAfter = longBreakAfter;
+      this.settings.longBreakDuration = longBreakDuration * 60;
+      this.remainingSeconds = this.settings.sessionDuration;
+      this.updateInterfaceTime();
+    }
   }
-
   
+  const timer = new Timer(document.querySelector(".timer"));
   
-  new Timer(
-      document.querySelector(".timer")
-  );
-
+  // Function to open the settings popup and set the input values based on current settings
+  function openSettings() {
+    const { sessionDuration, breakDuration, longBreakAfter, longBreakDuration } = timer.settings;
+  
+    document.getElementById("sessionDuration").value = sessionDuration / 60;
+    document.getElementById("breakDuration").value = breakDuration / 60;
+    document.getElementById("longBreakAfter").value = longBreakAfter;
+    document.getElementById("longBreakDuration").value = longBreakDuration / 60;
+  
+    document.getElementById("settingsPopup").style.display = "flex";
+  }
+  
+  // Function to save the settings from the popup and update the timer
+  function saveSettings() {
+    const sessionDuration = parseInt(document.getElementById("sessionDuration").value);
+    const breakDuration = parseInt(document.getElementById("breakDuration").value);
+    const longBreakAfter = parseInt(document.getElementById("longBreakAfter").value);
+    const longBreakDuration = parseInt(document.getElementById("longBreakDuration").value);
+  
+    timer.setSettings(sessionDuration, breakDuration, longBreakAfter, longBreakDuration);
+  
+    document.getElementById("settingsPopup").style.display = "none";
+  }
+  function closeSettings() {
+    document.getElementById("settingsPopup").style.display = "none";
+}
